0b6176f5cf040bedb41696abf3b6e8f76a39af53
Ruslan0920 <114057714+Ruslan0920@users.noreply.github.com>
2023-08-14 07:05:24 +0000

    Deploying to gh-pages from @ Ruslan0920/goit-js-hw-09@82824ce8f851698d6176d7986397eb9fb4fe0100 🚀

---
+++ 01-color-switcher.dc6828a8.js
@@ -0,0 +1,2 @@
+document.querySelector(data-start);
+//# sourceMappingURL=01-color-switcher.dc6828a8.js.map
---
+++ 01-color-switcher.e1e9e644.js
@@ -0,0 +1,2 @@
+document.querySelector(data-start);
+//# sourceMappingURL=01-color-switcher.e1e9e644.js.map
