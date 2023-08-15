5e7d3a8687e9ab10b14be46435f44dc2eaa4a6ad
Ruslan0920 <114057714+Ruslan0920@users.noreply.github.com>
2023-08-15 12:41:10 +0000

    Deploying to gh-pages from @ Ruslan0920/goit-js-hw-09@4345316c341fc055581d29dd0c6baecdac015096 🚀

--- 01-color-switcher.19fe7c66.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("button[data-start]").addEventListener("click",(function(t){console.log(t)}));
-//# sourceMappingURL=01-color-switcher.19fe7c66.js.map
--- 01-color-switcher.bcae6118.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("button[data-start]").addEventListener("click",(function(t){console.log(t)}));
-//# sourceMappingURL=01-color-switcher.bcae6118.js.map
