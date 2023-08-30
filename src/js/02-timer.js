903e15c6fd648521c5dc0dc5b77cc1a8db029068
Руслан <rus_boy2002@ukr.net>
2023-08-30 21:21:16 +0300

    many upd 02

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,5 +1,4 @@
 import flatpickr from 'flatpickr';
-console.log(flatpickr);
 
 import 'flatpickr/dist/flatpickr.min.css';
 
@@ -16,19 +15,11 @@ import Notiflix from 'notiflix';
 // // };
 
 const inputCalendar = document.querySelector('#datetime-picker');
-console.log(inputCalendar);
 const clickStartButton = document.querySelector('button[data-start]');
-console.log(clickStartButton);
 const valueData = document.querySelector('span[data-days]');
-console.log(valueData);
 const valueHours = document.querySelector('span[data-hours]');
-console.log(valueHours);
 const valueMinutes = document.querySelector('span[data-minutes]');
-console.log(valueMinutes);
 const valueSeconds = document.querySelector('span[data-seconds]');
-console.log(valueSeconds);
-// const timerClock = document.querySelector('.value');
-// console.log(timerClock);
 
 clickStartButton.addEventListener('click', () => {
   timer.start();
@@ -41,19 +32,37 @@ const currentTime = Date.now();
 const timer = {
   start() {
     const startTime = selectDateInput.selectedDates[0];
-    // console.log(startTime);
 
-    setInterval(() => {
+const timerId = setInterval(() => {
       const currentTime = Date.now();
       // console.log(currentTime);
       const deltaTime = startTime - currentTime;
       // console.log(deltaTime);
       const time = convertMs(deltaTime);
+      // console.log(time);
 
       updateClock(time);
-    }, 1000);
+}, 1000);
+    // clearInterval(timerId);
   },
 };
+
+const selectDateInput = flatpickr(inputCalendar, {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+    if (selectedDates[0] <= currentTime) {
+      Notiflix.Notify.failure('Please choose a date in the future');
+    } else {
+      clickStartButton.removeAttribute('disabled', 'disabled');
+    }
+    
+  },
+});
+console.log(selectDateInput.selectedDates[0]);
 
 function updateClock({ days, hours, minutes, seconds }) {
   valueData.textContent = `${days}`;
@@ -89,20 +98,3 @@ function convertMs(ms) {
 // console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
 // console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
 // console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
-
-const selectDateInput = flatpickr(inputCalendar, {
-  enableTime: true,
-  time_24hr: true,
-  defaultDate: new Date(),
-  minuteIncrement: 1,
-  onClose(selectedDates) {
-    console.log(selectedDates[0]);
-    if (selectedDates[0] <= currentTime) {
-      Notiflix.Notify.failure('Please choose a date in the future');
-      // window.alert("Please choose a date in the future");
-    } else {
-      clickStartButton.removeAttribute('disabled', 'disabled');
-    }
-  },
-});
-console.log(selectDateInput.selectedDates[0]);
