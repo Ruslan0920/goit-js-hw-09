87a9be805ac92ec8fd6df79e5b940efe0e12a9fd
Руслан <rus_boy2002@ukr.net>
2023-09-06 00:17:37 +0300

    up03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -7,10 +7,10 @@ function createPromise(position, delay) {
   }
 }
 
-createPromise(2, 1500)
-  .then(({ position, delay }) => {
-    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  })
-  .catch(({ position, delay }) => {
-    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-  });
+// createPromise(2, 1500)
+//   .then(({ position, delay }) => {
+//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//   })
+//   .catch(({ position, delay }) => {
+//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+//   });
