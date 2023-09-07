10acd817b85d28599ed5b237f5820e68b32df403
Руслан <rus_boy2002@ukr.net>
2023-09-08 00:27:09 +0300

    upd 03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,16 +1,36 @@
+const inputDelay = document.querySelector('[name="delay"]');
+console.log(inputDelay);
+console.log(inputDelay.value);
+const inputStep = document.querySelector('[name="step"]');
+console.log(inputStep);
+const inputAmount = document.querySelector('[name="amount"]');
+console.log(inputAmount);
+const buttonCreate = document.querySelector('[type="submit"]');
+buttonCreate.addEventListener('click', createPromise);
+console.log(buttonCreate);
+
+
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  inputAmount.value = position;
+  inputStep.value = delay;
+  const promise = new Promise((resolve, reject) => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        // Fulfill
+        resolve(console.log());
+      } else {
+        // Reject
+  reject(console.log())
+      }
+  })
+return promise;
 }
 
-// createPromise(2, 1500)
-//   .then(({ position, delay }) => {
-//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-//   })
-//   .catch(({ position, delay }) => {
-//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-//   });
+createPromise(2, 1500)
+  .then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
